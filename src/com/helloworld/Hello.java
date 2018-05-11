6d5bfa45482bd876778a5cbd8d8b5803c58e8728
Liangzanyang <1113433290@qq.com>
2018-05-11 10:06:30 +0800

    function_1

--- src/com/helloworld/Hello.java
+++ src/com/helloworld/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("my first Github test!");
+		System.out.println("this is function_one");
+
 	}
 
 }
